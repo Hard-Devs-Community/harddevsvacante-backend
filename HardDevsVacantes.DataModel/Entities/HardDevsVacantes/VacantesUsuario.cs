445c9d5d1f99d74139138c9b15803050f2111556
Jose Daniel Acosta Segura <josedanielacostasegura@gmail.com>
2025-02-09 21:56:52 -0400

    added, Rol and user antities, also UserConfiguration

---
+++ HardDevsVacantes.DataModel/Entities/HardDevsVacantes/VacantesUsuario.cs
@@ -0,0 +1,21 @@
+﻿using HardDevsVacantes.DataModel.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HardDevsVacantes.DataModel.Entities.HardDevsVacantes
+{
+	public class VacantesUsuario
+	{
+		public int Id { get; set; }
+		public string Nombre { get; set; } = default!;
+		public string Contrasena { get; set; } = default!;
+		public string Username { get; set; } = default!;
+		public string Email { get; set; } = default!;
+
+		public int RolId { get; set; }
+		public Rol? Rol { get; set; } 
+	}
+}
---
+++ HardDevsVacantes.DataModel/EntityConfiguration/HardDevsVacantes/UsuarioVacantesConfiguration.cs
@@ -0,0 +1,39 @@
+﻿using HardDevsVacantes.DataModel.Entities;
+using HardDevsVacantes.DataModel.Entities.HardDevsVacantes;
+using HardDevsVacantes.DataModel.Enums;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HardDevsVacantes.DataModel.EntityConfiguration.HardDevsVacantes
+{
+	public class UsuarioVacantesConfiguration : IEntityTypeConfiguration<VacantesUsuario>
+	{
+		public void Configure(EntityTypeBuilder<VacantesUsuario> builder)
+		{
+			builder.ToTable("Usuario");
+
+			builder.HasKey(x => x.Id);
+
+			builder.Property(x => x.Nombre)
+				.HasMaxLength(225)
+				.IsRequired();
+
+			builder.Property(x => x.Contrasena)
+				.HasMaxLength(225)
+				.IsRequired();
+
+			builder.Property(x => x.Username)
+				.HasMaxLength(225)
+				.IsRequired();
+
+			builder.HasOne(x => x.Rol)
+				.WithMany()
+				.HasForeignKey(x => x.RolId);
+		}
+	}
+}
